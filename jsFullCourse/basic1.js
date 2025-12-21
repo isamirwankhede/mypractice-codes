0470ee1733cdc2b792ab460b00bd326e73690228
Samir Wankhede <contactsamirw@gmail.com>
2025-12-21 07:02:03 +0000

    conversion of datatype

--- jsFullCourse/basic1.js
+++ jsFullCourse/basic1.js
@@ -5,14 +5,22 @@ let convertString = String(score)
 console.log(convertString);
 console.log(typeof convertString);
 
-let someString =  "444"
+let someString =  "444abc"
 let convertNumber = Number(score)
 console.log(typeof convertNumber);
 
-let some = ""
+//  string 33 => Number 
+//  string "33aa" => NaN 
+//  string true , false => 1 and 0
 
-let convertBoolean = Boolean(some)
-console.log(convertBoolean);
+let isLoggedIn = true;
+
+let booleanIntoNumber = Number(isLoggedIn)
+console.log(booleanIntoNumber);
+console.log(typeof booleanIntoNumber);
+
+// bool true => 1 
+// bool false => 0 
 
 
 
