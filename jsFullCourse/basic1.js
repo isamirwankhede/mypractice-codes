61d52d39e118d8bc50bf7e93441f22e669c9d101
Samir Wankhede <contactsamirw@gmail.com>
2025-12-21 06:51:41 +0000

    data types basic

---
+++ jsFullCourse/basic1.js
@@ -0,0 +1,18 @@
+let score = 33
+
+let convertString = String(score)
+
+console.log(convertString);
+console.log(typeof convertString);
+
+let someString =  "444"
+let convertNumber = Number(score)
+console.log(typeof convertNumber);
+
+let some = ""
+
+let convertBoolean = Boolean(some)
+console.log(convertBoolean);
+
+
+
