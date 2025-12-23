5f06dae85803924e83a841d4eae58ae2cd3373ca
Samir Wankhede <contactsamirw@gmail.com>
2025-12-23 05:35:45 +0000

    objects basics understand

--- jsFullCourse/basics/objects.js
+++ jsFullCourse/basics/objects.js
@@ -1,4 +1,7 @@
 
+objects : singalton
+// objects  literals
+
 const mySym  = Symbol("myKey1")
 
 const user = {
@@ -20,8 +23,7 @@ const user = {
 user.email = "samir@microsoft.com"
 console.log(user["email"]);
 
-// after freeze we cant able to do modifications/ anything
-Object.freeze(user)
+// after freeze we cant able to do modifications/ anything// Object.freeze(user)
 
 user.email = "samir@amazon.com"
 console.log(user);
@@ -32,5 +34,17 @@ user.greeting = function () {
     
 }
 
-console.log(user.greeting );
+user.greetingTwo = function () {
+    console.log(`hello ,${user.name}`)
+}
+
+console.log(user.greeting());
+console.log(user.greetingTwo());
+
+
+
+
+
+
+
 
